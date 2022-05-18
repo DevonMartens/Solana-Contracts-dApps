1bd1e4bd25c11e46a97ec49ce5e58c86ebd74b3e
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-17 20:57:16 -0400

    keys print

--- Airdrop/index.js
+++ Airdrop/index.js
@@ -9,3 +9,11 @@
 //new creates a new object & keypair An account keypair used for signing transactions.
 const wallet = new Keypair()
 //gets creditials of wallet
+
+//get public key
+const publicKey = wallet._keypair.publicKey
+//get secret key
+const secretKey = wallet._keypair.secretKey
+//view
+console.log(publicKey)
+console.log(secretKey)
