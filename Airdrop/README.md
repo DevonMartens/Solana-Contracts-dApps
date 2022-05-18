16eb0beebf6fce9576b66e2675f38d2032f29cf3
Devon Martens <devonmartens@MacBook-Pro.local>
2022-05-17 21:28:05 -0400

    finishes airdrop

--- Airdrop/README.md
+++ Airdrop/README.md
@@ -3,3 +3,29 @@
 ---
 
 ### This project allows airdropping using web3.js/solana. It is simply proof of concept.
+
+## Tools 
+
+* @solana/web3.js
+
+## Usage
+
+1. Creates public and private keys - these can be seen from console.logs in index.js on line 20 & 21.
+
+2. Retrives the balance of the wallet. This means creating a new instance of a public key changing line 14 to line 16. See below:
+
+   ```javascript
+   const publicKey = (wallet._keypair.publicKey)*/
+   //create new public key
+   const publicKey = new PublicKey(wallet._keypair.publicKey)
+   ```
+
+This will print out:
+
+   ```shell
+   Wallet Balance is 0
+   ```
+
+This is because there are no funds until you airdrop funds into the wallet. 
+
+3. The `airDropSol` functions gives 2 sol to the wallet as such the `main` functions calls it and checks the balance a second time to confirm.
--- Airdrop/index.js
+++ Airdrop/index.js
@@ -10,10 +10,57 @@
 const wallet = new Keypair()
 //gets creditials of wallet
 
-//get public key
-const publicKey = wallet._keypair.publicKey
+/*get public key
+const publicKey = (wallet._keypair.publicKey)*/
+//create new public key
+const publicKey = new PublicKey(wallet._keypair.publicKey)
 //get secret key
 const secretKey = wallet._keypair.secretKey
 //view
 console.log(publicKey)
 console.log(secretKey)
+/*view balance with getBalance from web3.js's connection class
+this is an async function with a try catch block 
+The try statement allows you to define a block of code to be tested for errors while it is being executed
+The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
+*/
+/* Connection is used to interact with the Solana JSON RPC. You can use Connection to confirm transactions, get account info, and more.
+
+You create a connection by defining the JSON RPC cluster endpoint and the desired commitment. 
+Once this is complete, you can use this connection object to interact with any of the Solana JSON RPC API.*/
+const getWalletBalance = async() => {
+    try{
+        //devnet is the a devnet fake solana. Passes clusterApiUrl into connection object to get it's details
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        //to specify where we are getting the balance from - calling public key from uptop
+        const walletBalance = await connection.getBalance(publicKey)
+        //results
+        console.log(`Wallet Balance is ${walletBalance}`)
+    } catch(err){
+        console.log(err)
+    }
+}
+/*Note: The current maximum for airdrops on devnet is usually limited to 1 SOL,
+ so you can just change the requestAirdrop line to*/
+
+//This function airdrops "sol" into the wallet
+const airDropSol = async() => {
+    try{
+        //create a connection object for the function
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        //passes in public key as 1st argument 2 amount of "sol"(units in lambhorts - 2 sol total)
+        const fromAirDropSigniture = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+        //confirms transaction
+        await connection.confirmTransaction(fromAirDropSigniture)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+ //function allowing us to run code
+ const main = async() => {
+     await getWalletBalance()
+     await airDropSol()
+     await getWalletBalance()   
+ }
+ main()
