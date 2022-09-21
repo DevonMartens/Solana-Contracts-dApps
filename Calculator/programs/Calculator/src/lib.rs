a3f5ce232c4e89c763867c8011600bbedd1ac218
Devon <dmmartens91@gmail.com>
2022-09-21 11:17:12 -0400

    adds struct and create fn

--- Calculator/programs/Calculator/src/lib.rs
+++ Calculator/programs/Calculator/src/lib.rs
@@ -25,4 +25,30 @@ pub mod calculator {
 }
 // user defined function called initalize and when it is called the program exits
 //most basic anchor program
-
+/*
+Use derive accounts Macro to indicate that this is a context
+* inside the {} we have info on the accounts we want to use
+* need an account for calculator - we create that using the init macro 
+INIT means - create new (create a new calculator)
+payer=user mean that the payment in sol will come from the user of the dApp
+SPACE argument means the amount of space to be allocated on chain for the calculator account
+*/
+#derive(Accounts)
+//create function
+pub struct create<'info>, {
+    #[account(init, payer=user, space=264)]
+    pub calculator: Account<'info Calculator>,
+    //makes user arg mutable 
+    #[account(mut)]
+    //user is signer b/c to create account they must sign tx
+    pub user: Signer<'info>,
+    //system specifications of solana blockchain
+    pub system_program: Program<'info, System>
+}
+//track 3 things greeting(init_message) msg mathmatical op result and the remainder when dividing
+#[account]
+pub struct Calculator{
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64
+}
